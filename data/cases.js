6d93398221dd1b16294a3ba2268471c8b78c5fcc
jljsj <liangjun.jiang@alipay.com>
2016-04-11 21:33:19 +0800

    Updates

--- data/cases.js
+++ data/cases.js
@@ -1,5 +1,7 @@
 module.exports = {
+  'src/cases/about.md': require('antd-md?fileName=src/cases/about.md!../../src/cases/about.md'),
   'src/cases/banner.md': require('antd-md?fileName=src/cases/banner.md!../../src/cases/banner.md'),
+  'src/cases/help.md': require('antd-md?fileName=src/cases/help.md!../../src/cases/help.md'),
   'src/cases/home.md': require('antd-md?fileName=src/cases/home.md!../../src/cases/home.md'),
   'src/cases/list.md': require('antd-md?fileName=src/cases/list.md!../../src/cases/list.md'),
 };
