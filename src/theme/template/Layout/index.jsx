06d38339e77bf7ad098557edc48e8cd002434f02
jljsj <liangjun.jiang@alipay.com>
2016-07-21 14:44:53 +0800

    update ease

--- src/theme/template/Layout/index.jsx
+++ src/theme/template/Layout/index.jsx
@@ -23,8 +23,8 @@ class Layout extends React.Component {
     return (<div id="react-root">
       <Header activeKey={pathKey} />
       <TweenOne.TweenOneGroup className="content-wrapper"
-        enter={{ y: '+=30', type: 'from', opacity: 0, ease: 'easeOutCirc'}}
-        leave={{ y: '+=30', opacity:0, ease: 'easeInOutCirc' }}
+        enter={{ y: '+=30', type: 'from', opacity: 0, ease: 'easeOutQuart'}}
+        leave={{ y: '+=30', opacity:0, ease: 'easeInOutQuart' }}
       >
         {children}
       </TweenOne.TweenOneGroup>
