15b2b94063b54468f4ef263ca1dcce4fcc88825f
jljsj <liangjun.jiang@alipay.com>
2016-07-21 11:51:28 +0800

    Updates

---
+++ data/cases.js
@@ -0,0 +1,10 @@
+module.exports = {
+  'src/cases/about.md': require('../../src/cases/about.md'),
+  'src/cases/banner.md': require('../../src/cases/banner.md'),
+  'src/cases/content.md': require('../../src/cases/content.md'),
+  'src/cases/footer.md': require('../../src/cases/footer.md'),
+  'src/cases/help.md': require('../../src/cases/help.md'),
+  'src/cases/home.md': require('../../src/cases/home.md'),
+  'src/cases/list.md': require('../../src/cases/list.md'),
+  'src/cases/nav.md': require('../../src/cases/nav.md'),
+};
