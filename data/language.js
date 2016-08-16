52d7f3c8c31101e4d3632d8f665f60c5efca9552
jljsj <liangjun.jiang@alipay.com>
2016-08-16 17:31:46 +0800

    Updates

--- data/language.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  'src/language/aware.md': require('../../src/language/aware.md'),
-  'src/language/interact.md': require('../../src/language/interact.md'),
-  'src/language/moving.md': require('../../src/language/moving.md'),
-  'src/language/space.md': require('../../src/language/space.md'),
-  'src/language/time.md': require('../../src/language/time.md'),
-  'src/language/transition.md': require('../../src/language/transition.md'),
-};
