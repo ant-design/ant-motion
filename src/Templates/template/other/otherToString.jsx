c31bf3a4ab27b4b44220ca28dce0ae84e408e635
jljsj <liangjun.jiang@alipay.com>
2016-09-18 11:43:40 +0800

    rename templates

--- src/Templates/template/other/otherToString.jsx
+++
@@ -1,9 +0,0 @@
-const index = require('!raw!./index.text');
-let point = require('!raw!./Point.jsx');
-point = point.replace('../static/point.less', './less/point.less');
-const documentation = require('!raw!./documentation.text');
-export default {
-  index,
-  documentation,
-  point,
-};
