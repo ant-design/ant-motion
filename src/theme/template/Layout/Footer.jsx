86d8235314a17d00d75a17e79918708a9174cc2d
jljsj <liangjun.jiang@alipay.com>
2017-01-22 15:35:21 +0800

    update footer

--- src/theme/template/Layout/Footer.jsx
+++ src/theme/template/Layout/Footer.jsx
@@ -9,16 +9,19 @@ function Footer(props) {
           <a target="_blank" href="https://github.com/ant-motion/ant-motion">仓库</a>
         </div>
         <div key="0">
-          <a target="_blank" href="https://github.com/ant-design/antd-init">antd-init</a> - 脚手架
+          <a target="_blank" href="https://github.com/dvajs/dva">dva</a> - 应用框架
         </div>
-        <div key="1"><a target="_blank" href="http://ant-tool.github.io/">ant-tool</a> - 开发工具</div>
+        <div key="1">
+          <a target="_blank" href="https://github.com/dvajs/dva-cli">dva-cli</a> - 脚手架
+        </div>
+        <div key="2"><a target="_blank" href="http://ant-tool.github.io/">ant-tool</a> - 开发工具</div>
       </li>
       <li>
         <h2 key="h2">相关站点</h2>
         <div key="0"><a target="_blank" href="http://ant.design/">Ant Design</a></div>
         <div key="1"><a target="_blank" href="http://mobile.ant.design">Ant Design Mobile</a> - 移动版</div>
-        <div key="2"><a target="_blank" href="https://g2.alipay.com/">G2</a> - 数据可视化</div>
-        <div key="3"><a target="_blank" href="https://antv.alipay.com/">AntV</a> - 数据可视化规范</div>
+        <div key="2"><a target="_blank" href="https://antv.alipay.com/">G2 AntV</a> - 数据可视化</div>
+        <div key="3"><a target="_blank" href="http://antdlibrary.alipay.net/">AntD Library</a> - Axure 部件库</div>
         <div key="4"><a target="_blank" href="http://ux.ant.design">Ant UX</a> - 页面逻辑素材</div>
       </li>
       <li>
