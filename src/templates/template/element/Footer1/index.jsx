7d9f0dbcb539cb9e3b4abff710dad469532d5431
jljsj <liangjun.jiang@alipay.com>
2017-02-28 17:32:45 +0800

    update footer1 end

--- src/templates/template/element/Footer1/index.jsx
+++ src/templates/template/element/Footer1/index.jsx
@@ -7,7 +7,6 @@ import './index.less';
 class Footer extends React.Component {
   static propTypes = {
     id: React.PropTypes.string,
-    dataSource: React.PropTypes.object,
   };
 
   static defaultProps = {
@@ -23,8 +22,8 @@ class Footer extends React.Component {
       })
       .map((key, i) => {
         const data = dataSource[key];
-        const links = data.children.contentLink.children.split(/\n/).filter(item => item);
-        const content = data.children.content.children.split(/\n/).filter(item => item)
+        const links = data.children.contentLink.split(/\n/).filter(item => item);
+        const content = data.children.content.split(/\n/).filter(item => item)
           .map((item, ii) => {
             const cItem = item.trim();
             const isImg = cItem.match(/\.(jpg|png|svg|bmp|jpeg)$/i);
@@ -35,33 +34,34 @@ class Footer extends React.Component {
             </li>);
           });
         return (<li className={data.className} key={i} id={`${this.props.id}-${key.split('_')[1]}`}>
-          <h2>{data.children.title.children}</h2>
-          <QueueAnim component="ul" type="bottom">
+          <h2>{data.children.title}</h2>
+          <ul>
             {content}
-          </QueueAnim>
+          </ul>
         </li>);
       });
 
   render() {
-    const dataSource = this.props.dataSource;
     const props = { ...this.props };
+    const dataSource = props.dataSource;
     const names = props.id.split('_');
     const name = `${names[0]}${names[1]}`;
+    const isMode = props.isMode;
     delete props.dataSource;
+    delete props.isMode;
     const logoContent = dataSource[`${name}_logo`].children;
     const liChildrenToRender = this.getLiChildren(dataSource);
     return (<OverPack
       {...props}
-      playScale={0.2}
+      playScale={isMode ? 0.5 : 0.2}
       hideProps={{ copyright: { reverse: true } }}
-      style={dataSource[name].style}
     >
-      <QueueAnim type="bottom" component="ul" key="ul" leaveReverse>
-        <li key="logo" id={`${props.id}-logo`} style={dataSource[`${name}_logo`].style}>
-          <p className="logo" style={logoContent.img.style} >
-            <img src={logoContent.img.children} width="100%" />
+      <QueueAnim type="bottom" component="ul" key="ul" leaveReverse id={`${props.id}-ul`}>
+        <li key="logo" id={`${props.id}-logo`}>
+          <p className="logo">
+            <img src={logoContent.img} width="100%" />
           </p>
-          <p style={logoContent.content.style}>{logoContent.content.children}</p>
+          <p>{logoContent.content}</p>
         </li>
         {liChildrenToRender}
       </QueueAnim>
@@ -69,7 +69,6 @@ class Footer extends React.Component {
         animation={{ y: '+=30', opacity: 0, type: 'from' }}
         key="copyright"
         className="copyright"
-        style={dataSource[`${name}_content`].style}
         id={`${props.id}-content`}
       >
         <span
