85af70ffb591dc1cd28cf439b8ccafd3f07adcbf
jljsj33 <liangjun.jiang@alipay.com>
2018-09-30 11:19:16 +0800

    update language copy

--- src/theme/template/other/Principle.jsx
+++ src/theme/template/other/Principle.jsx
@@ -3,9 +3,9 @@ import { Row, Col } from 'antd/lib/grid';
 import './principle.less';
 
 const text = [
-  { title: '自然', img: 'https://gw.alipayobjects.com/zos/rmsportal/LyTPSGknLUlxiVdwMWyu.gif', content: '自然运动规律，保证视觉连惯，让用户感知到动作是成立的' },
+  { title: '自然', img: 'https://gw.alipayobjects.com/zos/rmsportal/LyTPSGknLUlxiVdwMWyu.gif', content: '自然运动规律，保证视觉连惯，让用户感知到动作是自然的' },
   { title: '高效', img: 'https://gw.alipayobjects.com/zos/rmsportal/SQOZVQVIossbXpzDmihu.gif', content: '尽量节省过渡的时间，快速完成过渡的动画效果' },
-  { title: '克制', img: 'https://gw.alipayobjects.com/zos/rmsportal/OkIXkscKxywYLSrilPIf.gif', content: '做有意义的动效，不去做太多的修饰和干扰用户' },
+  { title: '克制', img: 'https://gw.alipayobjects.com/zos/rmsportal/OkIXkscKxywYLSrilPIf.gif', content: '做有意义的动效，不去做太多的修饰而干扰用户' },
 ];
 
 export default class Principle extends React.Component {
